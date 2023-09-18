749f5eebd5e64b567c2cccc5ef2cecddb30dd05c
vatsal-jogi <143814940+vatsal-jogi@users.noreply.github.com>
2023-09-18 12:32:50 +0530

    Write a PHP script to swap the values of two variables $x and $y using a temporary  variable. Display the swapped values using the echo statement.

--- script_7.php
+++ script_7.php
@@ -1,6 +1,10 @@
 <?php
-$word1 =" hello ";
-$word2= " you ";
-$word3= " all. ";
-echo "this is sentence  " .$word1 . $word2 . $word3;
+$a=7;
+$b=4;
+echo " before swap</br> " ." a= " . $a . " b= " . $b . "</br>" . "</br>";
+$c;
+$c=$b;//4
+$b=$a;//7
+$a=$c;//4
+echo "after swap </br>" . "a= " . $a . " b= " . $b; 
 ?>
